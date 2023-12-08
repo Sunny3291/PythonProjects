8bd5bd6af3886dc00f8d2e65abffa634e08a1e79
Sunny3291 <145218554+Sunny3291@users.noreply.github.com>
2023-12-08 09:45:20 +0530

    Add files via upload

---
+++ conditional_operatiors.py
@@ -0,0 +1,44 @@
+# If
+
+# '''         # Syntax
+
+# # if(condition):
+# #     print("Print Statement")
+
+# # else:
+# #     print("Print Statement")  '''
+    
+    
+
+# a=int(input("Enter The first Value "))
+# b=int(input("Enter The Second Value "))
+
+# if(a-b>0): 
+#     print("Postive Substraction is:",a-b)
+
+# else:
+#     print("Postive Substraction is:",b-a)
+
+
+
+# a=10
+# b=20
+# c=30
+
+# if (a==20):
+#     print("a is 10")
+
+# elif(b==20):
+#     print("b is 20")
+
+    
+#netseted if statement
+
+# if(condition):
+#     if(con):
+#         if(con):
+#             print("statement")
+#         else
+#     else
+# else
+
